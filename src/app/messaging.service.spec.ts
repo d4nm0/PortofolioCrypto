7ddb6d70561b316b03dd6f9f87683412494beeea
lexbal <alexballe35@gmail.com>
2021-12-16 10:43:19 +0100

    update

---
+++ src/app/messaging.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { environment } from 'src/environments/environment';
+import { MessagingService } from './messaging.service';
+
+describe('MessagingService', () => {
+  let service: MessagingService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireDatabaseModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        AngularFireMessagingModule,
+        AngularFireAuthModule,
+       ],
+      providers: [ MessagingService ],
+    });
+    service = TestBed.inject(MessagingService);
+  });
+
+
+  it('Devrait créer le component', () => {
+    expect(service).toBeTruthy();
+  });
+});
