2fd276ac9248b5bb8a52a917d0471baafe6c2855
Dan Monceau <dan.monceau.lpcb@gmail.com>
2022-01-15 23:35:07 +0100

    ajout de capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.danmonceau.fr',
+  appName: 'portofolio-crypto',
+  webDir: 'dist/PortofolioCrypto',
+  bundledWebRuntime: false
+};
+
+export default config;
